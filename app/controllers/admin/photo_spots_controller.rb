9dd1595d6b46b8d7dbb6886a36d8bee81e933289
Mr-T18 <itsuki.tkhs@gmail.com>
2025-08-27 04:34:32 +0000

    キーワード検索機能とタグ追加

--- app/controllers/admin/photo_spots_controller.rb
+++ app/controllers/admin/photo_spots_controller.rb
@@ -59,6 +59,6 @@ private
   end
 
   def photo_spot_params
-    params.require(:photo_spot).permit(:name, :address, :detail, :parking_flag)
+    params.require(:photo_spot).permit(:name, :address, :detail, :parking_flag, { tag_ids: [] })
   end
 end
--- app/controllers/photo_spots_controller.rb
+++ app/controllers/photo_spots_controller.rb
@@ -1,5 +1,6 @@
 class PhotoSpotsController < ApplicationController
   def index
     @photo_spots = PhotoSpot.all
+    @photo_spots = @photo_spots.search_by_keyword(params[:q]) if params[:q].present?
   end
 end
