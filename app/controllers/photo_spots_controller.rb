31ec0695da98dc1b9542840b2dec648f08653422
Mr-T18 <itsuki.tkhs@gmail.com>
2025-08-27 07:18:47 +0000

    なんか惜しい（できてない）

--- app/controllers/photo_spots_controller.rb
+++ app/controllers/photo_spots_controller.rb
@@ -3,6 +3,7 @@ class PhotoSpotsController < ApplicationController
   def index
     @photo_spots = PhotoSpot.all
     @photo_spots = @photo_spots.search_by_keyword(params[:q]) if params[:q].present?
+    @photo_spots = @photo_spots.search_by_keyword(params[:tags]) if params[:tags].present?
   end
 
   def show
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[8.0].define(version: 2025_08_27_051955) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
@@ -81,6 +80,11 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_27_051955) do
 
   create_table "tags", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
