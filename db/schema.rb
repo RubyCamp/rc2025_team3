d005951dc786f4be2f919d178cfa4b75fa835df0
takami <souta.miyu2328@gmail.com>
2025-08-28 01:21:59 +0000

    カラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_27_080447) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_28_004531) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -63,6 +63,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_27_080447) do
     t.decimal "latitude", precision: 10, scale: 7
     t.decimal "longitude", precision: 10, scale: 7
     t.string "tags"
+    t.string "season"
+    t.time "timestart"
+    t.time "timeend"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -72,11 +75,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_27_080447) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["photo_spot_id"], name: "index_reviews_on_photo_spot_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
